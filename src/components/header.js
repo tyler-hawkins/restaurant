e7e48f28a9e4ff362be474c5f1767937d797b527
Tyler Hawkins <udlr121@gmail.com>
2021-11-12 03:19:16 -0500

    Add stylesheet

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,9 @@
+import "../style.css";
+
 function createHeader() {
 	const header = document.createElement("div")
 	header.classList.add("header");
+
 	const title = document.createElement("h2");
 	title.innerHTML = "Restaurant";
 	header.appendChild(title);
